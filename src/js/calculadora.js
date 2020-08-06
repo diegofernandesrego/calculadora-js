4fbdefd43a6891f4168a2ec13e7bedb7cd6f23b3
C1310640 Diego Fernandes do Rego <c1310640@interno.bb.com.br>
2020-08-06 14:45:08 -0300

    Implementando função de subtração

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -7,12 +7,18 @@ var Calculadora = {
         num2 = parseFloat(num2);
         return num1 + num2;
     },
+
     subtrair: function(num1,num2){
-        return 0;
+        if(isNaN(num1) || isNaN(num2)){
+            return 0;
+        }
+        return num1 - num2;
     },
+
     dividir: function(num1,num2){
         return 0;
     },
+
     multiplicar: function(num1,num2){
         return 0;
     }
